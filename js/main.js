02ed16077f0ceca4761e563a95748a9ab097b5bd
Karpenko Nataliia <netaly79@gmail.com>
2019-07-12 14:49:52 +0300

    burger-menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-var swiper = new Swiper('.swiper-container', {
+$(document).ready(function(){
+
+  var x=0;
+
+  $('.nav-toggle').on('click', function(e){
+    e.preventDefault();
+    $('.burger-menu ul').toggleClass('is-open');	
+    if (x%2===0) {
+      $('.nav-toggle').css("margin-bottom", "80px");
+      x=x+1;
+    }
+    else{
+      $('.nav-toggle').css("margin-bottom", "0px");
+      x=x+1;
+    };
+  })
+
+  var swiper = new Swiper('.swiper-container', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  });
+    });
+})
+
+
