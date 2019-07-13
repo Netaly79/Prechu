96e301ce3683f9e08aad942afe632b7fb8450c58
Karpenko Nataliia <netaly79@gmail.com>
2019-07-13 21:51:09 +0300

    new burger

--- js/main.js
+++ js/main.js
@@ -5,14 +5,7 @@ $(document).ready(function(){
   $('.nav-toggle').on('click', function(e){
     e.preventDefault();
     $('.burger-menu ul').toggleClass('is-open');	
-    if (x%2===0) {
-      $('.nav-toggle').css("margin-bottom", "80px");
-      x=x+1;
-    }
-    else{
-      $('.nav-toggle').css("margin-bottom", "0px");
-      x=x+1;
-    };
+   
   })
 
   var swiper = new Swiper('.swiper-container', {
